4687aa08516d5a975b466dc9497817cf353a4edf
Balram Pandey <balrampandey19@gmail.com>
2016-05-05 19:07:31 +0530

    with picture

--- index.js
+++ index.js
@@ -27,6 +27,7 @@ app.post('/webhook', function (req, res) {
         var event = events[i];
         if (event.message && event.message.text) {
             sendMessage(event.sender.id, {text: "Echo: " + event.message.text});
+        kittenMessage(event.sender.id,"aadesh");
         }
     }
     res.sendStatus(200);
