4002367c06aad2add8aa1870653bf99926e62b3e
Balram Pandey <balrampandey19@gmail.com>
2016-05-06 12:50:20 +0530

    remove ii

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 var express = require('express');
 var bodyParser = require('body-parser');
 var request = require('request');
-var parsedJSON = require('./path/games.json');
+
 var app = express();
 
 app.use(bodyParser.urlencoded({extended: false}));
