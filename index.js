cf417aa151b1fbf156c64304b969ccfafee587a1
Balram Pandey <balrampandey19@gmail.com>
2016-05-07 11:39:05 +0530

    Messanger Bo

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
 var express = require('express');
 var bodyParser = require('body-parser');
 var request = require('request');
-var parsedJSON = require('./path/games.json');
 var app = express();
 
 app.use(bodyParser.urlencoded({extended: false}));
