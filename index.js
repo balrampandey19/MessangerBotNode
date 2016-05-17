da35b0e593a70054ebe60c62dc61495191b5adf2
Balram Pandey <balrampandey19@gmail.com>
2016-05-17 11:59:20 +0530

    user id

--- index.js
+++ index.js
@@ -38,7 +38,7 @@ app.post('/webhook', function (req, res) {
 });
 function processMessage(recipientId,text)
 {
-    console.log(recipientId);
+    console.log("recipientId"+recipientId);
 
     
     var message = [
